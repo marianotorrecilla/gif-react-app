0cdd9efeffd26f5aacc1b7f38e42fa15ddce7d77
ihsa-mariano <mtorrecilla@emergencias.com.ar>
2021-08-27 15:01:27 -0300

    perf: ListOfGifs component
    update ListOfGifs component and styles
    apply new path structure

---
+++ src/components/ListOfGifs/ListOfGifs.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Gif from '../Gif/Gif'
+import './ListOfGifs.css'
+
+export default function ListOfGifs ({gifs}) {
+    return (
+        <div className='ListOfGifs'>
+            {
+                gifs.map(({id, title, url}) => 
+                    <Gif
+                        key={id}
+                        id={id}
+                        title={title} 
+                        url={url}  
+                    />
+                )
+            }
+        </div>
+    )
+}
