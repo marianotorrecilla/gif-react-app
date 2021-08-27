efaf807f7686bce03831ab92b183c3e617bbc17f
ihsa-mariano <mtorrecilla@emergencias.com.ar>
2021-08-27 20:01:33 -0300

    perf: useGifs hook
    add pagination into hook

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -2,18 +2,22 @@ import { useContext, useEffect, useState} from 'react'
 import getGifs from '../services/getGifs'
 import GifsContext from '../context/GifsContext'
 
+const INITIAL_PAGE = 0
+
 export default function useGifs ({ keyword } = {keyword: null}) {
     const [loading, setLoading] = useState(false)
+    const [loadingNextPage, setLoadingNextPage] = useState(false)
+    const [page, setPage] = useState(INITIAL_PAGE)
     const {gifs, setGifs} = useContext(GifsContext)
 
-    useEffect(function () {
-        setLoading(true)
-
-        /**
+    /**
          * keywordToUse constant to retrieve the keyword of localStorage
          */
 
-        const keywordToUse = keyword ? keyword : localStorage.getItem('lastKeyword') || 'random'
+    const keywordToUse = keyword ? keyword : localStorage.getItem('lastKeyword') || 'random'
+
+    useEffect(function () {
+        setLoading(true)
 
         getGifs({ keyword: keywordToUse })
             .then(gifs => {
@@ -26,7 +30,19 @@ export default function useGifs ({ keyword } = {keyword: null}) {
 
                 localStorage.setItem('lastKeyword', keyword)
             })    
-    }, [keyword, setGifs])
+    }, [keyword, keywordToUse, setGifs])
 
-    return {loading, gifs}
+    useEffect(function () {
+        if (page === INITIAL_PAGE) return
+
+        setLoadingNextPage(true)
+
+        getGifs({ keyword: keywordToUse, page })
+            .then(nextGifs => {
+                setGifs(prevGifs => prevGifs.concat(nextGifs))
+                setLoadingNextPage(false)
+            })
+    }, [keywordToUse, page, setGifs])
+
+    return {loading, loadingNextPage, gifs, setPage}
 }
