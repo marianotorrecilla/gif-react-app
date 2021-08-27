b801d7828bc1c0635f93d3c9879ffd16e55b0aa1
ihsa-mariano <mtorrecilla@emergencias.com.ar>
2021-08-27 19:52:20 -0300

    feat: category component
    create category component and styles

---
+++ src/components/Category/Category.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'wouter'
+import './Category.css'
+
+export default function Category({name, options = []}){
+    return (
+        <>
+            <div className="Category-div">
+                <h3 className="Category-title">{name}</h3>
+                <ul className="Category-ul">
+                    {options.map((singleOption) => (
+                        <li className="Category-li" key={singleOption}>
+                            <Link className="Category-link" to={`/search/${singleOption}`}>{singleOption}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            
+        </>
+    )
+}
