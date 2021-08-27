ec00e5c39859ae38142234b123dafbf6b0b49971
ihsa-mariano <mtorrecilla@emergencias.com.ar>
2021-08-27 19:58:27 -0300

    perf: getGifs service
    update service with reusable constants api key and api url

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,19 +1,22 @@
-const apiKey = 'jowx15DDeh2zDmjwrKmz2Xni2O3NWl4i'
+import {API_KEY, API_URL} from './settings'
 
-export default function getGifs ({keyword = 'friends'} = {}) {
-    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=25&offset=0&rating=g&lang=en`
+const fromApiResponseToGifs = apiResponse => {
+    const {data = []} = apiResponse
+    if (Array.isArray(data)) {
+        const gifs = data.map(image => {
+            const {images, title, id} = image
+            const {url} = images.downsized_medium
+            return { title, id, url }
+        })
+        return gifs
+    }
+    return []
+}
+
+export default function getGifs ({keyword = 'friends', limit = 25, page = 0} = {}) {
+    const apiUrl = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=g&lang=en`
 
     return fetch(apiUrl)
         .then(res => res.json())
-        .then(response => {
-            const {data} = response
-            if (Array.isArray(data)) {
-            const gifs = data.map(image => {
-                const {images, title, id} = image
-                const {url} = images.downsized_medium
-                return { title, id, url }
-            })
-            return gifs
-            }
-        })
+        .then(fromApiResponseToGifs)
 }
