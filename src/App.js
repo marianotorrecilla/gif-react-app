e7cb5de8358443826921602a1f74bd3a8a4050be
ihsa-mariano <mtorrecilla@emergencias.com.ar>
2021-08-27 15:08:10 -0300

    perf: App.js imports
    add new pages and context to application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import './App.css';
-import ListOfGifs from './components/ListOfGifs';
+import Home from './pages/Home/index'
+import SearchResults from './pages/SearchResults/index'
+import Detail from './pages/Detail/index'
+import StaticContext from './context/StaticContext';
+import { GifsContextProvider } from './context/GifsContext';
 
 import { Link, Route } from "wouter"
 
 function App() {
   return (
-    <div className="App">
-      <section className="App-content">
-        <h1>Giffy App</h1>
-        <Link to='/gif/phoebe'>Gifs Phoebe</Link>
-        <Link to='/gif/joey'>Gifs Joey</Link>
-        <Link to='/gif/chandler'>Gifs Chandler</Link>
-        <Link to='/gif/monica'>Gifs Mónica</Link>
-        <Link to='/gif/rachel'>Gifs Rachel</Link>
-        <Link to='/gif/ross'>Gifs Ross</Link>
-        <Route
-          path="/gif/:keyword"
-          component={ListOfGifs}
-        />
-      </section>
-    </div>
+    <StaticContext.Provider value={{ name:'marianotorrecilla' }}>
+      <div className="App">
+        <section className="App-content">
+          <Link to="/">
+            <h1 style={{cursor: 'pointer'}}>Gif React App</h1>
+          </Link>
+          <GifsContextProvider>
+            <Route 
+              component={Home}
+              path="/"
+            />
+            <Route
+              component={SearchResults}
+              path="/search/:keyword"
+            />
+            <Route
+              component={Detail}
+              path="/gif/:id"
+            />
+          </GifsContextProvider>
+        </section>
+      </div>
+    </StaticContext.Provider>
   );
 }
 
