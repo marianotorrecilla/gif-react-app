9c16adecfbe0e57fa8889d3f537eadeabe154506
ihsa-mariano <mtorrecilla@emergencias.com.ar>
2021-08-31 17:42:50 -0300

    perf: search form on home page
    import search form component in home page
    add useCallback to perform handlesubmit on searches

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,36 +1,24 @@
-import React, {useState} from 'react'
-import ListOfGifs from '../../components/ListOfGifs/ListOfGifs'
+import React, { useCallback } from 'react'
 import TrendingSearches from '../../components/TrendingSearches/TrendingSearches'
+import SearchForm from '../../components/SearchForm/SearchForm'
+import ListOfGifs from '../../components/ListOfGifs/ListOfGifs'
 import { useLocation } from 'wouter'
 import useGifs from '../../hooks/useGifs'
 import '../../App.css'
 
+
 export default function Home () {
-    const [keyword, setKeyword] = useState('')
     const [path, pushLocation] = useLocation()
     const {loading, gifs} = useGifs()
 
-    const handleSubmit = e => {
-        e.preventDefault()
+    const handleSubmit = useCallback(({keyword}) => {
         pushLocation(`/search/${keyword}`)
-    }
-
-    const handleChange = e => {
-        setKeyword(e.target.value)
-    }
+    }, [pushLocation])
 
     return (
         <>
             <TrendingSearches />
-            <form onSubmit={handleSubmit}>
-                <input
-                    className="App-search-box"
-                    onChange={handleChange} 
-                    placeholder="search a gif here..." 
-                    type='text' 
-                    value={keyword} 
-                />
-            </form>
+            <SearchForm onSubmit={handleSubmit}/>
             <h3 className="App-title">Last search</h3>
             <ListOfGifs gifs={gifs} />
         </>
