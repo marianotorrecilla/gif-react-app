95e8903c4b270a2c6a6a70a2a51a69ce876b9173
ihsa-mariano <mtorrecilla@emergencias.com.ar>
2021-08-31 11:30:15 -0300

    perf: lazy home
    add lazy load to Home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import Home from './pages/Home/index'
 import SearchResults from './pages/SearchResults/index'
 import Detail from './pages/Detail/index'
 import StaticContext from './context/StaticContext';
@@ -8,29 +7,34 @@ import { GifsContextProvider } from './context/GifsContext';
 
 import { Link, Route } from "wouter"
 
+const HomePage = React.lazy(() => import('./pages/Home'))
+
 function App() {
   return (
     <StaticContext.Provider value={{ name:'marianotorrecilla' }}>
       <div className="App">
-        <section className="App-content">
-          <Link to="/">
-            <h1 style={{cursor: 'pointer'}}>Gif React App</h1>
-          </Link>
-          <GifsContextProvider>
-            <Route 
-              component={Home}
-              path="/"
-            />
-            <Route
-              component={SearchResults}
-              path="/search/:keyword"
-            />
-            <Route
-              component={Detail}
-              path="/gif/:id"
-            />
-          </GifsContextProvider>
-        </section>
+        <Suspense fallback={null} >
+          <section className="App-content">
+            <Link to="/">
+              <h1 style={{cursor: 'pointer'}}>Gif React App</h1>
+            </Link>
+            <GifsContextProvider>
+              <Route 
+                component={HomePage}
+                path="/"
+              />
+              <Route
+                component={SearchResults}
+                path="/search/:keyword"
+              />
+              <Route
+                component={Detail}
+                path="/gif/:id"
+              />
+            </GifsContextProvider>
+          </section>
+        </Suspense>
+        
       </div>
     </StaticContext.Provider>
   );
